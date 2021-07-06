86f246d6227b02e47ac221235560f07c98a4c982
Volkova-Kate <ea.volkova@skbkontur.ru>
2021-07-07 00:38:27 +0700

    update

---
+++ UserAPI/Models/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace UserAPI.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        //[BsonElement("FirstName")]
+        public string FirstName { get; set; }
+        //[BsonElement("LastName")]
+        public string LastName { get; set; }
+        //[BsonElement("Number")]
+        public string Number { get; set; }
+    }
+}
+ 
---
+++ UserAPI/Services/UserService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using UserAPI.Models;
+
+
+namespace UserAPI.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UserService(IUserstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            //считывает экземпляр сервера для выполнения операций с базой данных
+            var database = client.GetDatabase(settings.DatabaseName);
+            //представляет базу данных Mongo для выполнения операций
+
+            _users = database.GetCollection<User>(settings.UsersCollectionName);
+
+            //var ls = Get();
+        }
+
+        public List<User> Get() =>
+            _users.Find(user => true).ToList();
+
+        public User Get(string id) =>
+            _users.Find<User>(user => user.Id == id).FirstOrDefault();
+
+        public User Create(User user)
+        {
+            _users.InsertOne(user);
+            return user;
+        }
+
+        public void Update(string id, User userIn) =>
+            _users.ReplaceOne(user => user.Id == id, userIn);
+
+        public void Remove(User userIn) =>
+            _users.DeleteOne(user => user.Id == userIn.Id);
+
+        public void Remove(string id) =>
+            _users.DeleteOne(user => user.Id == id);
+    }
+}
