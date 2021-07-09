3414a1c60f15f9f89c331b71702a4023dbda632a
Volkova-Kate <ea.volkova@skbkontur.ru>
2021-07-09 11:50:54 +0700

    api with validation and pagination

---
+++ UserAPI/Controllers/UsersController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using UserAPI.Models;
+using UserAPI.Services;
+
+namespace UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UsersController(UserService userService)
+        //использует класс BookService для выполнения операций CRUD;
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<User>> Get() =>
+            _userService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetUser")]
+        public ActionResult<User> Get(string id)
+        {
+            var user = _userService.Get(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpGet("page")]
+        public ActionResult<List<User>> GetPage(string skip, string count)
+        {
+            if (!int.TryParse(skip, out var skipValue) || !int.TryParse(count, out var countValue))
+                return NotFound();
+            return _userService.GetPage(skipValue, countValue);
+        }
+
+        [HttpPost]
+        public ActionResult<User> Create(User user)
+        {
+            _userService.Create(user);
+
+            return CreatedAtRoute("GetUser", new { id = user.Id.ToString() }, user);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, User userIn)
+        {
+            var user = _userService.Get(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _userService.Update(id, userIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var user = _userService.Get(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _userService.Remove(user.Id);
+
+            return NoContent();
+        }
+
+    }
+}
